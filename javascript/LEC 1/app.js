00bf6c1bc66799be87b5f0845d5d4503d7557935
Priyansh Gupta <priyanshgupta1227@gmail.com>
2023-10-11 11:19:54 +0530

    first commit

---
+++ javascript/LEC 1/app.js
@@ -0,0 +1,18 @@
+// console.log(1===1);
+// console.log(1==1);
+// console.log('1'==1);
+// console.log('1'===1);
+// console.log(true==1);
+// console.log(true===1);
+// console.log(true=='1');
+// console.log(true=='true');
+// console.log(true=='');
+// console.log(false=='');
+// console.log(false==[]);
+// console.log(true=='');
+
+
+
+
+
+
