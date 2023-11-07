9189909d1977b9007977ab71b92454456dc52f12
Priyansh Gupta <priyanshgupta1227@gmail.com>
2023-11-07 11:26:17 +0530

    first commit

---
+++ LEC 28/01_Scope.js
@@ -0,0 +1,14 @@
+// {
+//     let a = 20;
+// }
+// console.log(a);
+
+// var name = 'priyansh';
+// var name = 'priyansh 1';
+// let name = 'akash';
+// let name = 'sachin';
+
+
+let  name = 'priyansh';
+name = 'priyansh 2';
+console.log(name);
