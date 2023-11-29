68eee7146aac012c4297102534e2f47423e39609
Priyansh Gupta <priyanshgupta1227@gmail.com>
2023-11-29 11:04:32 +0530

    first commit

---
+++ LEC 33/DOM_Intro/app.js
@@ -0,0 +1,22 @@
+// const ele = document.getElementsByTagName('div');
+
+// const ele = document.getElementsByClassName('child');
+// console.log(ele);
+// console.log(ele[0]);
+
+// const ele = document.getElementById('parent');
+// console.log(ele);
+
+// const ele = document.querySelector('div');
+// const ele = document.querySelector('.child');
+// const elel = document.querySelector('#parent');
+// console.log(elel);
+// console.log(ele);
+
+const ele = document.querySelectorAll('.child');
+console.log(ele);
+
+// ele[0].innerText = 'priyansh';
+console.log(ele[0].innerText);
+console.log(ele[1].textContent);
+
