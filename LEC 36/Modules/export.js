16d7798e4150e04d9366917eefad93d1d5074f49
Priyansh Gupta <priyanshgupta1227@gmail.com>
2023-12-05 11:49:19 +0530

    first commit

---
+++ LEC 36/Modules/export.js
@@ -0,0 +1,5 @@
+export let firstname = 'priyansh'
+export let lastname = 'gupta'
+
+let val = 99;
+export default val;
---
+++ LEC 36/Modules/import.js
@@ -0,0 +1,6 @@
+import {firstname, lastname} from './export.js';
+
+console.log(firstname, lastname);
+
+import val1 from "./export.js"
+console.log(val1)
